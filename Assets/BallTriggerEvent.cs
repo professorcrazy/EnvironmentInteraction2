1375b1607a1256caf58264f77ef02ef1f18ac9a5
professorcrazy <58807701+professorcrazy@users.noreply.github.com>
2020-01-10 08:56:26 +0100

    Ball trigger events
    Ball trigger events

--- Assets/BallTriggerEvent.cs
+++ Assets/BallTriggerEvent.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BallTriggerEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Color targetNewColor = Color.cyan;
+    public GameObject target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    int count = 0;
+    [SerializeField] int triggerCount = 3;
+
+    [SerializeField] GameObject explosion;
+
+    private void OnTriggerEnter(Collider other) {
+        count++;
+        if (count % triggerCount == 0 ) {
+            target.GetComponent<Renderer>().material.color = new Color(
+                                                       Random.Range(0f, 1f),
+                                                       Random.Range(0f, 1f),
+                                                       Random.Range(0f, 1f));
+        }
+        GameObject tempExplosion = Instantiate(explosion, target.transform.position, 
+                                         target.transform.rotation);
+        Destroy(tempExplosion, 2f);
     }
+    
 }
